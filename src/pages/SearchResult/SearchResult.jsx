871c829112e4c18211f32db30ea80451e1532ea9
aligbakhtiyar <ahmadbakhtiyar@gmail.com>
2023-06-17 14:08:03 +0530

    SearchResult

--- src/pages/SearchResult/SearchResult.jsx
+++ src/pages/SearchResult/SearchResult.jsx
@@ -1,12 +1,43 @@
-import React from 'react';
-import './style.scss';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import InfiniteScroll from "react-infinite-scroll-component";
+
+import "./style.scss";
+
+import { fetchDataFromApi } from "../../utils/api";
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
+// import MovieCard from "../../components/movieCard/MovieCard";
+// import Spinner from "../../components/spinner/Spinner";
+import noResults from "../../assets/no-results.png";
 
 const SearchResult = () => {
-  return (
-    <div>
-      SearchReasiult
-    </div>
-  )
-}
+  const [data, setData] = useState(null);
+  const [pageNum, setPageNum] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const { query } = useParams();
 
-export default SearchResult
+  const fetchInitialData = () => {
+    setLoading(true);
+    fetchDataFromApi(`search/multi?query=${query}&page=${pageNum}`).then(
+      (res) => {
+        setData(res);
+        setPageNum((prev) => prev + 1);
+        setLoading(false);
+      }
+    );
+  };
+
+
+  const fetchNextPageData = () => {
+    fetchDataFromApi(`/search/multi?query=${query}&page=${pageNum}`)
+  }
+
+  useEffect(() => {
+    setPageNum(1);
+    fetchInitialData();
+  }, [query]);
+
+  return <div className="searchResultsPage"></div>;
+};
+
+export default SearchResult;
