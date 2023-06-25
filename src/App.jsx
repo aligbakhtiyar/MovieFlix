9599313c2c9fec47a7641f11eb8d5003f1e8e5c3
aligbakhtiyar <ahmadbakhtiyar@gmail.com>
2023-06-25 08:53:20 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { fetchDataFromApi } from "./utils/api";
 import { useSelector, useDispatch } from "react-redux";
 import { getApiConfiguration } from "./store/homeSlice";
 import Header from "./components/header/Header";
-import Footer from "./components/footer/Footer";
+
 
 // import all pages & components
 
@@ -44,7 +44,6 @@ function App() {
         <Route path="/search/:query" element={<SearchResult />} />
         <Route path='/:mediaType/:id' element ={<Details/>} />
       </Routes>
-      {/* <Footer/> */}
     </BrowserRouter>
   );
 }
